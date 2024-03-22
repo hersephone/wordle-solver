8bf621654dd9aa1260c2505c5638f9415ee661b1
hersephone <164571030+hersephone@users.noreply.github.com>
2024-03-22 17:50:58 -0400

    Create wordle.py

---
+++ wordle.py
@@ -0,0 +1,30 @@
+def spellcheck(green, black, yellow, word):
+    for t in range (5):
+        if word[t] in black and word[t] not in green: return False
+        for u in range (5):
+            if word[t] in yellow[u][t]: return False
+            if yellow[u][t] != "!":
+                if yellow[u][t] not in word: return False
+        if green[t] != "!":
+            if word[t] != green[t]: return False
+    return True
+
+wrdlreader = open('wordle.txt', 'r')
+
+wrdl = []
+for line in wrdlreader:
+    wrdl.append(line.strip())
+wrdlreader.close()
+
+black = "" #black letters
+yellow = [""] * 5
+yellow[0] = "!!!!!" #replace ! with yellow letters from first row, leave the rest
+yellow[1] = "!!!!!" #yellow letters from second row
+yellow[2] = "!!!!!" #yellow letters from third row
+yellow[3] = "!!!!!" #yellow letters from fourth row
+yellow[4] = "!!!!!" #yellow letters from fifth row
+green = "!!!!!" #replace ! with green letters, leave the rest
+
+for x in range (0, len(wrdl)):
+    if spellcheck(green, black, yellow, wrdl[x]) == False: continue
+    print(wrdl[x])
