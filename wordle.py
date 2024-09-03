a841f5966c96e8c22fe9b6aee6f29d36d15db7d3
hersephone <164571030+hersephone@users.noreply.github.com>
2024-09-03 13:01:46 -0400

    updated 09/03/2024
    comments added

--- wordle.py
+++ wordle.py
@@ -1,36 +1,34 @@
-def spellcheck(green, black, yellow, word):
+def spellcheck(green, black, yellow, word): #function to evaluate a candidate word
     for t in range (5):
-        if word[t] in black and word[t] not in green: return False
+        if word[t] in black: return False #if a letter is in black return false
+        if green[t] != "!": if word[t] != green[t]: return False #if a green letter isn't in the word return false
         for u in range (5):
-            if word[t] in yellow[u][t]: return False
-            if yellow[u][t] != "!":
-                if yellow[u][t] not in word: return False
-        if green[t] != "!":
-            if word[t] != green[t]: return False
+            if word[t] in yellow[u][t]: return False #if a letter is in an indicated wrong position return false
+            if yellow[u][t] != "!": if yellow[u][t] not in word: return False #if a yellow letter isn't in the word return false
     return True
 
-wrdlreader = open('wordle.txt', 'r')
+wrdlreader = open('wordle.txt', 'r') #open list of candidate words
 
 wrdl = []
 for line in wrdlreader:
-    wrdl.append(line.strip())
+    wrdl.append(line.strip()) #read in candidate words
 wrdlreader.close()
 
-black = "" #black letters
+black = ""
 yellow = [""] * 5
-yellow[0] = "!!!!!" #replace ! with yellow letters from first row, leave the rest
-yellow[1] = "!!!!!" #yellow letters from second row
-yellow[2] = "!!!!!" #yellow letters from third row
-yellow[3] = "!!!!!" #yellow letters from fourth row
-yellow[4] = "!!!!!" #yellow letters from fifth row
-green = "!!!!!" #replace ! with green letters, leave the rest
+yellow[0] = "!!!!!" 
+yellow[1] = "!!!!!" 
+yellow[2] = "!!!!!" 
+yellow[3] = "!!!!!" 
+yellow[4] = "!!!!!" 
+green = "!!!!!"
 
 for y in range(0, 5):
-    black += input("Black letters?\n")
-    green = input("Green letters?\n")
+    black += input("Black letters?\n") #take input for black letters
+    green = input("Green letters?\n") #take input for green letters
     if green == "": green = "!!!!!"
-    yellow[y] = input("Yellow letters?\n")
+    yellow[y] = input("Yellow letters?\n") #take input for yellow letters
     if yellow[y] == "": yellow[y] = "!!!!!"
     for x in range (0, len(wrdl)):
-        if spellcheck(green, black, yellow, wrdl[x]) == False: continue
-        print(wrdl[x])
+        if spellcheck(green, black, yellow, wrdl[x]) == False: continue #skip invalid candidate words
+        print(wrdl[x]) #print valid candidate words
